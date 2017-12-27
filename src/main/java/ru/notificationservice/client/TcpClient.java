38b5b68f9b4e5b9bd614361af777f7494324b0db
Maksim Vinogradov <maxvgrad@gmail.com>
2017-12-27 22:53:01 +0300

    Add notification field
    Delete all data for outputStream.write() into Notification class

--- src/main/java/ru/notificationservice/client/TcpClient.java
+++ src/main/java/ru/notificationservice/client/TcpClient.java
@@ -2,15 +2,12 @@ package ru.notificationservice.client;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.notificationservice.utils.NotificationType;
+import ru.notificationservice.notification.Notification;
 
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.time.LocalTime;
-import java.util.UUID;
-
-import static ru.notificationservice.utils.NotificationProtocol.*;
+import java.util.Objects;
 
 
 public class TcpClient {
@@ -18,67 +15,35 @@ public class TcpClient {
     private static final Logger log = LoggerFactory.getLogger(TcpClient.class);
 
     private Socket socket;
-    private OutputStream output;
 
-    private String id;
-    private String message;
-    private String time;
-    private NotificationType notificationType;
-    private String extraParam;
+    private Notification notification;
 
-    public TcpClient(NotificationType notificationType, String message, String time, String extraParam) {
-        id = UUID.randomUUID().toString();
-        this.message = message;
-        this.time = time;
-        this.notificationType = notificationType;
-        this.extraParam = extraParam;
-        log.debug("{} setup is done");
+    private TcpClient() {
     }
 
-    public static void main(String[] args) {
-        LocalTime time = LocalTime.now().plusSeconds(15);
-        new TcpClient(NotificationType.MAIL
-                , "test msg1"
-                , time.toString(), "mrbamma@yandex.ru").addNotification();
-
-//        new TcpClient(NotificationType.HTTP
-//                , "TEST MSG"
-//                , time.toString()
-//                , "https://requestb.in/17409i51?inspect").addNotification();
+    public static TcpClient getInstance(Notification notification) {
+        TcpClient client = new TcpClient();
+        client.notification = Objects.requireNonNull(notification);
+        return client;
     }
 
     public void addNotification() {
+
         try {
+
             socket = new Socket("localhost", 8080);
 
-            log.debug("Socket {}", socket);
-            try {
-                output = socket.getOutputStream();
+            try(ObjectOutputStream output =
+                        new ObjectOutputStream(socket.getOutputStream())) {
 
-                output.write(String.format("%s=%s\r\n", EXTERNAL_ID, id).getBytes());
-                output.write(String.format("%s=%s\r\n", NOTIFICATION_TYPE, notificationType).getBytes());
-                output.write(String.format("%s=%s\r\n", MESSAGE, message).getBytes());
-                output.write(String.format("%s=%s\r\n", TIME, time).getBytes());
-                log.debug("before switch");
-                switch (notificationType) {
-                    case HTTP:
-                        output.write(String.format("%s=%s\r\n", URL, extraParam).getBytes());
-                        log.debug("HTTP case");
-                        break;
-                    case MAIL:
-                        log.debug("MAIL case");
-                        output.write(String.format("%s=%s\r\n", EMAIL, extraParam).getBytes());
-                }
+                output.writeObject(notification);
+
                 output.flush();
 
             } catch (IOException e) {
-                log.error("I/O error write");
-                throw new RuntimeException(e.getMessage());
-            } finally {
-                if (output != null) {
-                    output.close();
-                    log.debug("Closing output");
-                }
+                e.printStackTrace();
+                log.error("{}", e.getMessage());
+                throw new RuntimeException(e.getCause());
             }
 
         } catch (IOException e) {
